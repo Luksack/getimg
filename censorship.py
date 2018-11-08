a33bfc65ddf8e862d6071ebd18603432651dcdb5
Lukasz Sack <lukas.sack@gmail.com>
2018-11-08 13:32:41 +0100

    refactored code, new req added

--- censorship.py
+++ censorship.py
@@ -2,20 +2,24 @@ from profanity import profanity
 import os
 
 
-def is_nice(word):
-    custom_bad_words = []
+def profane_words():
     current_file = os.path.abspath(os.path.dirname(__file__))
-    bad_words = os.path.join(current_file, './data/bad_words.txt')
+    bad_words = os.path.join(current_file, 'data', 'bad_words.txt')
     with open(bad_words) as input_file:
-        for line in input_file:
-            custom_bad_words.append(line.strip())
-    profanity.load_words(custom_bad_words)
+        bad_list = [line.strip() for line in input_file if line.startswith('a')]
+
+    return bad_list
+
+
+def verify_nice(word):
+    profanity.load_words(profane_words())
     words = word.replace(' ', '')
-    if words.isalpha():
-        if profanity.contains_profanity(word):
-            print('Err: contains profanity: %s' % word)
-        else:
-            # This is just placeholder to add any function later
-            print('word is ok')
-    else:
+    if not words.isalpha():
         print('Input can contain only letters')
+        raise Exception('Input can contain only letters')
+    if profanity.contains_profanity(word):
+        print('Err: contains profanity: %s' % word)
+        raise Exception('Input should not be a profanity')
+
+        # This is just placeholder to add any function later
+    print('word is ok')
