4e8919077aa0a2ebb1a103ed8e920f25dadcffa5
Lukasz Sack <lukas.sack@gmail.com>
2018-11-14 11:46:20 +0100

    shorter, cleaner code

--- main/censorship.py
+++ main/censorship.py
@@ -12,27 +12,19 @@ def profane_words():
 
 
 def remove_duplicates(word):
-    cleared_chars = []
+    cleared_word = word[0]
 
     for i in range(len(word)):
-        if i == 0:
-            cleared_chars += word[0]
-        else:
-            if word[i] not in cleared_chars[-1]:
-                cleared_chars += word[i]
-    new_word = ("".join(cleared_chars))
-    return new_word
+        if word[i] != cleared_word[-1]:
+            cleared_word += word[i]
+
+    return cleared_word
 
 
 def verify_nice(word):
     profanity.load_words(profane_words())
     clear_word = remove_duplicates(word)
-    # Radek said 'only keywords no spaces allowed'
     if not word.isalpha():
         raise Exception('Input can contain only letters')
-    if profanity.contains_profanity(word):
+    if any([w for w in [word, clear_word] if profanity.contains_profanity(w)]):
         raise Exception('Input should not be a profanity')
-    if profanity.contains_profanity(clear_word):
-        raise Exception('Input should not be a profanity duplicates')
-
-
