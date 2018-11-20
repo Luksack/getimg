41483840a0b16f9fbf3891b3bfb09d273406f44f
Lukasz Sack <lukas.sack@gmail.com>
2018-11-20 10:00:09 +0100

    tests for web.py

---
+++ tests/test_web.py
@@ -0,0 +1,28 @@
+import unittest
+from main import web
+
+
+class FirstTest(unittest.TestCase):
+    def setUp(self):
+        self.app = web.app.test_client()
+
+    def test_one(self):
+        req = self.app.get('/')
+        self.assertEqual(req.status, '200 OK')
+        self.assertIn('HyenaCORP Project', str(req.data))
+
+    def test_post_good_word(self):
+        data = {
+            'example': 'nice'
+        }
+        req = self.app.post('/', data=data)
+        self.assertEqual(req.status, '200 OK')
+        self.assertIn(f'Results for: {data["example"]}', str(req.data))
+
+    def test_post_bad_word(self):
+        data = {
+            'example': 'cock'
+        }
+        req = self.app.post('/', data=data)
+        self.assertEqual(req.status, '200 OK')
+        self.assertIn('Input should not be a profanity', str(req.data))
