c2309397b1834fe7f9d16a30f1cc0147bfdbeb53
Lukasz Sack <lukas.sack@gmail.com>
2018-12-07 16:03:19 +0100

    redirect to search-results page

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect, url_for
 
 from resources import censorship, get_image
 
@@ -11,12 +11,18 @@ app = Flask(__name__)
 def home_page():
     return render_template('index.html')
 
+@app.route('/search-results')
+def search_results():
+    project_dir = os.path.dirname(os.path.abspath(__file__))
+    directory = os.path.join(project_dir, 'static', 'images')
+    mylist = [f for f in os.listdir(directory) if not f.startswith('.')]
+
+    return render_template('search-results.html', mylist=mylist)
+
 
 @app.route('/', methods=['POST'])
 def user_input():
     image_amount = 10
-    project_dir = os.path.dirname(os.path.abspath(__file__))
-    directory = os.path.join(project_dir, 'static', 'images')
     text = request.form['query']
     try:
         censorship.verify_nice(text)
@@ -24,8 +30,7 @@ def user_input():
         return render_template('index.html', er=str(error), bad_results=text)
     else:
         get_image.ImageDownloader(text, image_amount)
-        mylist = [f for f in os.listdir(directory) if not f.startswith('.')]
-        return render_template('search-results.html', good_results=text, mylist=mylist)
+        return redirect(url_for('.search_results'))
 
 
 if __name__ == '__main__':
